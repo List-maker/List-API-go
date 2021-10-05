b521464bf6a9deda2876406cdc416a1159b401c1
Maxi-Mega <ixam.agem@gmail.com>
2021-10-05 19:57:27 +0200

    Reformat id placeholder in route definitions

--- src/routes.go
+++ src/routes.go
@@ -24,33 +24,33 @@ func initRoutes() *mux.Router {
 
 	// Users
 	r.HandleFunc("/api/user", users.AuthRequired(users.GetCurrentUser)).Methods(http.MethodGet)
-	r.HandleFunc("/api/user/{id}", users.GetUser).Methods(http.MethodGet)
+	r.HandleFunc("/api/user/{id:[0-9]+}", users.GetUser).Methods(http.MethodGet)
 	r.HandleFunc("/api/user", users.UserRequired(users.UpdateCurrentUser)).Methods(http.MethodPut)
 	// r.HandleFunc("/api/user/password", users.AuthRequired(users.UpdatePassword)).Methods(http.MethodPut)
-	r.HandleFunc("/api/user/{id}/avatar", users.GetAvatar).Methods(http.MethodGet)
+	r.HandleFunc("/api/user/{id:[0-9]+}/avatar", users.GetAvatar).Methods(http.MethodGet)
 	r.HandleFunc("/api/user/avatar", users.UserRequired(users.UpdateAvatar)).Methods(http.MethodPut)
 	r.HandleFunc("/api/user/avatar", users.UserRequired(users.DeleteAvatar)).Methods(http.MethodDelete)
 
 	// Lists
 	r.HandleFunc("/api/list", users.UserRequired(lists.CreateList)).Methods(http.MethodPost)
 	r.HandleFunc("/api/list/user", users.UserRequired(lists.GetUserLists)).Methods(http.MethodGet)
-	r.HandleFunc("/api/list/{id}", users.UserRequired(lists.GetList)).Methods(http.MethodGet)
-	r.HandleFunc("/api/list/{id}", users.UserRequired(lists.UpdateList)).Methods(http.MethodPut)
-	r.HandleFunc("/api/list/{id}/pin", users.UserRequired(lists.PinList)).Methods(http.MethodPut)
-	r.HandleFunc("/api/list/{id}", users.UserRequired(lists.DeleteList)).Methods(http.MethodDelete)
+	r.HandleFunc("/api/list/{id:[0-9]+}", users.UserRequired(lists.GetList)).Methods(http.MethodGet)
+	r.HandleFunc("/api/list/{id:[0-9]+}", users.UserRequired(lists.UpdateList)).Methods(http.MethodPut)
+	r.HandleFunc("/api/list/{id:[0-9]+}/pin", users.UserRequired(lists.PinList)).Methods(http.MethodPut)
+	r.HandleFunc("/api/list/{id:[0-9]+}", users.UserRequired(lists.DeleteList)).Methods(http.MethodDelete)
 
 	// Items
 	r.HandleFunc("/api/list/{list_id}/add", users.UserRequired(items.CreateItem)).Methods(http.MethodPost)
-	r.HandleFunc("/api/item/{id}", users.UserRequired(items.GetItem)).Methods(http.MethodGet)
-	r.HandleFunc("/api/item/{id}", users.UserRequired(items.UpdateItem)).Methods(http.MethodPut)
-	r.HandleFunc("/api/item/{id}/check", users.UserRequired(items.CheckItem)).Methods(http.MethodPut)
-	r.HandleFunc("/api/item/{id}", users.UserRequired(items.DeleteItem)).Methods(http.MethodDelete)
+	r.HandleFunc("/api/item/{id:[0-9]+}", users.UserRequired(items.GetItem)).Methods(http.MethodGet)
+	r.HandleFunc("/api/item/{id:[0-9]+}", users.UserRequired(items.UpdateItem)).Methods(http.MethodPut)
+	r.HandleFunc("/api/item/{id:[0-9]+}/check", users.UserRequired(items.CheckItem)).Methods(http.MethodPut)
+	r.HandleFunc("/api/item/{id:[0-9]+}", users.UserRequired(items.DeleteItem)).Methods(http.MethodDelete)
 
 	// Invitations
 	r.HandleFunc("/api/invitation/new", users.UserRequired(invitations.CreateInvit)).Methods(http.MethodPost)
 	r.HandleFunc("/api/invitation/list", users.UserRequired(invitations.ListInvits)).Methods(http.MethodGet)
-	r.HandleFunc("/api/invitation/{id}", users.UserRequired(invitations.GetInvit)).Methods(http.MethodGet)
-	r.HandleFunc("/api/invitation/{id}/accept", users.UserRequired(invitations.AcceptInvit)).Methods(http.MethodPut)
-	r.HandleFunc("/api/invitation/{id}/delete", users.UserRequired(invitations.DeleteInvit)).Methods(http.MethodPut)
+	r.HandleFunc("/api/invitation/{id:[0-9]+}", users.UserRequired(invitations.GetInvit)).Methods(http.MethodGet)
+	r.HandleFunc("/api/invitation/{id:[0-9]+}/accept", users.UserRequired(invitations.AcceptInvit)).Methods(http.MethodPut)
+	r.HandleFunc("/api/invitation/{id:[0-9]+}/delete", users.UserRequired(invitations.DeleteInvit)).Methods(http.MethodPut)
 	return r
 }
